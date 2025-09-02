ff9ff22b741c1ac77cf525ecd8dc772a4fcc356f
Peter Harrison <pmch2@cam.ac.uk>
2025-09-02 17:13:40 +0100

    Updating PsyNet

--- experiment.py
+++ experiment.py
@@ -13,14 +13,11 @@ from psynet.modular_page import (
     MultiRatingControl,
 )
 from psynet.page import InfoPage
-from psynet.timeline import Event, Timeline
+from psynet.timeline import Event, join
 from psynet.trial.static import StaticNode, StaticTrial, StaticTrialMaker
 
 # TODO: General dashboard cleaning
-# TODO: Reordering experiment.py so that the Experiment class comes first
-# TODO: Lighter Dockerfile (e.g. use uv base image)
 # TODO: Make AudioContext requesting less intrusive when it needs to happen (e.g. play button)
-# TODO: Better default text for audio replay button
 # TODO: Automatically waiting for the audio to complete before allowing submission
 # TODO: Automatically set cache=True for all assets that are available on deployment
 # TODO: Don't create static folder with psynet debug local (maybe revisit calling of _create_symlink)
@@ -34,39 +31,31 @@ from psynet.trial.static import StaticNode, StaticTrial, StaticTrialMaker
 N_TRIALS_PER_PARTICIPANT = 6
 
 
-def get_nodes():
-    return [
-        StaticNode(
-            definition={
-                "stimulus_name": stimulus["name"]
-            },
-            assets={
-                "stimulus_audio": asset(
-                    stimulus["path"],
-                    extension=".mp3",
-                    cache=True,  # reuse the uploaded file between deployments
-                )
-            },
+class Exp(psynet.experiment.Experiment):
+    label = "Subjective rating"
+
+    def get_timeline(self):
+        return join(
+            InfoPage(
+                """
+                In this experiment you will hear some sounds. Your task will be to rate
+                them on a scale of 1 to 5 on several scales.
+                """,
+                time_estimate=5,
+            ),
+            StaticTrialMaker(
+                id_="ratings",
+                trial_class=CustomTrial,
+                nodes=get_nodes,
+                expected_trials_per_participant=N_TRIALS_PER_PARTICIPANT,
+            ),
+            InfoPage(
+                """
+                Thank you for your participation!
+                """,
+                time_estimate=5,
+            ),
         )
-        for stimulus in list_stimuli()
-    ]
-
-def list_stimuli():
-    stimulus_dir = Path("data/instrument_sounds")
-    return [
-        {
-            "name": path.stem,
-            "path": path,
-        }
-        for path in list(stimulus_dir.glob("*.mp3"))
-    ]
-
-# Run `python3 experiment.py` to list the stimuli.
-if __name__ == "__main__":
-    stimuli = list_stimuli()
-    print(f"Found {len(stimuli)} stimuli:")
-    for stimulus in stimuli:
-        print(f"- {stimulus['name']}")
 
 
 class CustomTrial(StaticTrial):
@@ -102,28 +91,15 @@ class CustomTrial(StaticTrial):
             time_estimate=10,
         )
 
-
-class Exp(psynet.experiment.Experiment):
-    label = "Subjective rating"
-
-    timeline = Timeline(
-        InfoPage(
-            """
-            In this experiment you will hear some sounds. Your task will be to rate
-            them on a scale of 1 to 5 on several scales.
-            """,
-            time_estimate=5,
-        ),
-        StaticTrialMaker(
-            id_="ratings",
-            trial_class=CustomTrial,
-            nodes=get_nodes,
-            expected_trials_per_participant=N_TRIALS_PER_PARTICIPANT,
-        ),
-        InfoPage(
-            """
-            Thank you for your participation!
-            """,
-            time_estimate=5,
-        ),
-    )
+def get_nodes():
+    return [
+        StaticNode(
+            definition={
+                "stimulus_name": path.stem
+            },
+            assets={
+                "stimulus_audio": asset(path, cache=True),  # reuse the uploaded file between deployments
+            },
+        )
+        for path in Path("data/instrument_sounds").glob("*.mp3")
+    ]
